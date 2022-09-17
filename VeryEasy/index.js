86c2fdba9609b563413dea1641e96ff45bdc07e1
Hussnain Ali <52062338+SyedHussnain@users.noreply.github.com>
2022-09-18 01:42:07 +0500

    Challenge No.3

--- VeryEasy/index.js
+++ VeryEasy/index.js
@@ -4,3 +4,10 @@ function sum(a, b) {
   return a + b;
 }
 console.log("Return the sum of Two Numbers = " + sum(15, 32));
+
+// Challenge No.3
+// Convert Minutes into Seconds
+function minToSec(min) {
+    return min * 60;
+}
+console.log("Minutes in to Seconds = " + minToSec(60));
