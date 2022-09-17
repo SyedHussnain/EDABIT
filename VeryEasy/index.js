d88c39368381604249dfe732742851c21e217e70
Hussnain Ali <52062338+SyedHussnain@users.noreply.github.com>
2022-09-18 02:24:26 +0500

    Challenge No.4

--- VeryEasy/index.js
+++ VeryEasy/index.js
@@ -11,3 +11,10 @@ function minToSec(min) {
     return min * 60;
 }
 console.log("Minutes in to Seconds = " + minToSec(60));
+
+// Challenge No.4
+// Return the Next Number from the Integer Passed
+function nextNumber(x) {
+  return x + 1;
+}
+console.log("Next Number is = " + nextNumber(15));
