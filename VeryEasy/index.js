0a2da3c982ad500797336eb35b08d62c81dd3de8
SyedHussnain <i.em.hussnain@gmail.com>
2022-10-04 19:52:13 +0500

    Challenge No.86

--- VeryEasy/index.js
+++ VeryEasy/index.js
@@ -850,3 +850,11 @@ function concat(arr1, arr2) {
   return arr1.concat(arr2);
 }
 console.log(concat([1, 3, 5], [2, 6, 8]));
+
+// Challenge No.86
+// Find the Index
+function findIndex(arr, str) {
+  return arr.indexOf(str);
+}
+
+console.log(findIndex(["hi", "edabit", "fgh", "abc"], "abc"))
